186757267dd7edefea71477d7c61797dde2690f2
Evgenii Kondratiev <9219210217@yandex.ru>
2025-08-12 21:51:08 +0300

    изменен формат вопроса в brain_progression

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -16,11 +16,13 @@ def game():
     progression = [start + i * step for i in range(count)]
     index_to_replace = random.randint(0, count - 1)
     replaced_number = progression[index_to_replace]
-    progression[index_to_replace] = '...'
-    answer_str = input(f"Question: {progression} ")
+    progression_with_hole = progression.copy()
+    progression_with_hole[index_to_replace] = '..'
+
+    answer = input('Question: ' + ' '.join(str(x) for x in progression_with_hole) + " ")
 
     try:
-        answer = int(answer_str)
+        answer = int(answer)
     except ValueError:
         print(f'"{answer}" is wrong answer ;(. Correct answer was "{correct_answer}". Let\'s try again, {name}')
 
